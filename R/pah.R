e1842bc92c2ef2cd3100b2699266a7f0170957fd
Oliver <soliver@usgs.gov>
2017-12-21 14:51:49 -0600

    Describe data in data folder

---
+++ R/pah.R
@@ -0,0 +1,20 @@
+#' Default PAH pcodes
+#'
+#' A vector of pcodes related to PAH analysis identified in Baldwin et al. (2017).
+#'
+#' @name pah_pcodes
+#' @rdname sampleData
+#' @docType data
+#' @examples
+#' length(pah_pcodes)
+"pah_pcodes"
+
+#' A table of PAH compound names, abbreviations, pcodes, and supporting information such as
+#' molecular weights and whether the compound is a parent or alkylated compound.
+#'
+#' @name pah_compounds
+#' @rdname sampleData
+#' @docType data
+#' @examples
+#' head(pah_compounds)
+"pah_compounds"
