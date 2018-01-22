02c5f170a30f4033c35b446eee89cbda0a423e1f
Oliver <soliver@usgs.gov>
2018-01-22 10:38:06 -0600

    modify what is returned

--- R/pah_profiles.R
+++ R/pah_profiles.R
@@ -12,8 +12,11 @@
 #' CAS registration number ('casrn'), or compound name ('Compound').
 #' @param sources a dataframe of source profiles. The default is to use the built-in `source_profiles` table,
 #' but users can provide their own table. This is useful if the user has a source profile to add to the built-in table.
-#' @return Returns a long dataframe where observations are repeated for each sample/compound combination. Adds to the input
-#' dataframe all columns in `source_profiles`, as well as the chi-squared difference between source and sample.
+#' @return Returns two data frames. The first is a long dataframe where observations are repeated for each sample/compound combination. Adds to the input
+#' dataframe all columns in `source_profiles`, as well as the chi-squared difference between source and sample, and the proportional
+#' concentrations. The second dataframe is a long data frame where observations are repeated for
+#' each sample/source combination, and the chi2 value is summed across all compounds to create a distance
+#' metric between each sample and source profile.
 #' @import dplyr
 #' @importFrom rlang sym
 #' @examples
