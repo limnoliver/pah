6d4fed489adb4187f866580ae367ddf95ec077d8
Oliver <soliver@usgs.gov>
2018-01-23 09:22:37 -0600

    do not export profile plots for now

--- R/plot_profiles.R
+++ R/plot_profiles.R
@@ -1,10 +1,9 @@
 #' plot_profiles
 #'
-#' Creates common figures from the calculates sample and source profiles. Possible plots include
-#' a boxplot of the sum of the distance metric across all sources, proportional concentration
-#' profile figures comparing an individual sample to a source.
+#' Creates common figures from the calculated distance between sample and source profiles.
+#' Possible plots include a boxplot of the sum of the distance metric across all sources and
+#' proportional concentration profile figures comparing an individual sample to a source.
 #'
-#' @export
 #' @param profile_dat The output of `pah_profiler` which includes the proportional concentration
 #' profiles from the samples and sources, as well the sum of the distance metric for each
 #' sample/source combination.
@@ -23,37 +22,5 @@
 
 plot_profiles <- function(profile_dat, compound_column = 'casrn', sample_column = 'sample_id',
                          conc_column = 'RESULT', sources = source_profiles) {
-  # make column names dplyr-ready
-  quo_compound_column <- sym(compound_column)
-  quo_conc_column <- sym(conc_column)
-  quo_sample_column <- sym(sample_column)
 
-  # pull out all 12 compounds
-  profile_compounds <- select(sources, !!quo_compound_column)
-
-  # filter user samples to include only those in the source profiles
-  samp.prof <- filter(pah_dat, (!!quo_compound_column) %in% profile_compounds[[1]])
-
-  # group by sample - calculate sum total and whether or not there are any samples = 0
-  samp.prof.bysample <- group_by(samp.prof, !!quo_sample_column) %>%
-    summarize(total_pah = sum(!!quo_conc_column))
-
-  samp.prof <- left_join(samp.prof, samp.prof.bysample) %>%
-    mutate(prop_conc = (!!quo_conc_column)/total_pah)
-
-  # merge in source compound info
-  all.profs <- full_join(samp.prof, sources, by = compound_column) %>%
-    select(-RESULT) %>%
-    gather(key = source, value = source_prop_conc, -(1:8), -Compound, -Abbreviation, -pcode, -molwt)
-
-  # calculate the chi squared difference
-  all.profs <- mutate(all.profs, chi2 = (abs(prop_conc - source_prop_conc)^2)/((prop_conc + source_prop_conc)/2))
-
-  # sum over the sources by sample
-  all.summary <- group_by(all.profs, sample_id, source) %>%
-    summarize(sum_chi2 = sum(chi2))
-
-  out <- list(all.profs, all.summary)
-  names(out) <- c('profiles', 'sum_chi2')
-  return(out)
 }
