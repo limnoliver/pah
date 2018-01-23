51779551f2aab778d5838a448523001ae626ed79
Oliver <soliver@usgs.gov>
2018-01-23 14:39:56 -0600

    changes sources to sources_plot to avoid overlapping with temp data names

--- R/plot_profiles.R
+++ R/plot_profiles.R
@@ -4,23 +4,94 @@
 #' Possible plots include a boxplot of the sum of the distance metric across all sources and
 #' proportional concentration profile figures comparing an individual sample to a source.
 #'
+#' @export
 #' @param profile_dat The output of `pah_profiler` which includes the proportional concentration
 #' profiles from the samples and sources, as well the sum of the distance metric for each
-#' sample/source combination.
+#' sample/source combination. These are combined in a list.
 #' @param plot_type The desired plot type. This can either be a boxplot ('boxplot') of all distance measures
 #' by source, or various profile ('profile') plots.
-#' @param source If `plot_type` = `profile`, include a vector of sources that you want to include
-#' in a profile plot to compare to samples.
-#' @return Returns two data frames. The first is a long dataframe where observations are repeated for each sample/compound combination. Adds to the input
-#' dataframe all columns in `source_profiles`, as well as the chi-squared difference between source and sample, and the proportional
-#' concentrations. The second dataframe is a long data frame where observations are repeated for
-#' each sample/source combination, and the chi2 value is summed across all compounds to create a distance
-#' metric between each sample and source profile.
+#' @param sources_plot If `plot_type` = `profile`, include a vector of sources that you want to include
+#' in a profile plot to compare to samples. Set equal to "all" to include paneled plots of all source profiles.
+#' @param sample_column Column that contains unique sample IDs.
+#' @param samples If `plot_type` = `profile`, a unique sample ID to use to plot against source profiles.
+#' Can either be a single unique ID or "all" to indicate taking the mean and standard deviation of all samples.
+#' @param include_creosote Logical, whether to include the source profiles for creosote (n = 2). The source profiles
+#' for creosote only include 11 compounds, and the missing 12th compound will be dropped in all sample-source
+#' comparisons. Users should create plots both ways to determine if creosote is an important source. If it is
+#' not important (determined by using TRUE), then proceed using plots that ignore creosote profiles.
+#' @import ggplot2
 #' @import dplyr
 #' @importFrom rlang sym
 #' @examples
 
-plot_profiles <- function(profile_dat, compound_column = 'casrn', sample_column = 'sample_id',
-                         conc_column = 'RESULT', sources = source_profiles) {
+plot_profiles <- function(profile_dat, plot_type = 'boxplot', sources = NA, samples = 'all',
+                          sample_column = 'sample_id', include_creosote = F) {
+  quo_sample_column <- sym(sample_column)
+  if (plot_type == 'boxplot') {
+    if (include_creosote == F) {
+      # find and filter out sources = creosote
+      creosote <- unique(grep('creosote', profile_dat[[2]]$source, ignore.case = T, value = T))
+      sum_chi2 <- filter(profile_dat[[2]], !(source %in% creosote))
 
+      order.vals <- sum_chi2 %>%
+        group_by(source) %>%
+        summarize(med = median(sum_chi2)) %>%
+        arrange(med)
+
+      sum_chi2$source <- factor(sum_chi2$source, levels = order.vals$source)
+
+    } else {
+      sum_chi2 <- profile_dat[[2]]
+    }
+    # create boxplot
+    p <- ggplot(sum_chi2, aes(x = source, y = sum_chi2)) +
+      geom_boxplot() +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+      labs(x = '', y = 'Sum Chi2')
+
+  } else if (plot_type == 'profile') {
+    pro_dat <- profile_dat[[1]]
+
+    if (samples == 'all') {
+      # calculate sample means and sds if samples == 'all'
+      sample_pro_dat <- group_by(pro_dat, Compound) %>%
+        summarize(mean_prop_conc = mean(prop_conc),
+                  sd_prop_conc = sd(prop_conc)) %>%
+        rename(sample_prop_conc = mean_prop_conc)
+    } else {
+      samples_prop_dat <- filter(pro_dat, quo_sample_column == samples)
+    }
+    sources <- select(pro_dat, Compound, source, source_prop_conc) %>%
+      rename(prop_conc = source_prop_conc, profile = source) %>%
+      distinct()
+
+    if (include_creosote == F) {
+      sources <- filter(sources, !(profile %in% c('Creosote_railway_ties', 'Creosote_product')))
+    } else {
+      # if user wants to keep creosote source profiles, then drop compound that is missing
+      # from creosote profiles
+      sources <- filter(sources, Compound != 'benzo[e]pyrene')
+    }
+    profiles <- left_join(sources, sample_pro_dat) %>%
+      left_join(distinct(pro_dat[,c('Compound', 'molwt')]))
+
+    if (!('all' %in% sources_plot)){
+      profiles <- filter(profiles, profile %in% sources_plot)
+    }
+
+    p <- ggplot(profiles, aes(x = reorder(Compound, molwt), y = prop_conc)) +
+      geom_point(alpha = 0.7) +
+      geom_point(aes(x = reorder(Compound, molwt), y = sample_prop_conc), color = '#3182BD', alpha = 0.8) +
+      geom_errorbar(aes(ymin = sample_prop_conc-sd_prop_conc, ymax = sample_prop_conc+sd_prop_conc),
+                    color = '#3182BD', alpha = 0.7) +
+      facet_wrap(~profile, scales = 'free_y') +
+      theme_bw() +
+      labs(x = "", y = "Proportional Concentration") +
+      theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+            panel.grid.minor.y = element_blank())
+  } else {
+    warning('plot_type does not equal "boxplot" or "profile"')
+  }
+  return(p)
 }
