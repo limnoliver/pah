c7ef09c3f19f37d3052e73ebdb396cfdd97cd265
Oliver <soliver@usgs.gov>
2018-01-24 14:47:52 -0600

    script to calculate ratios from samples that match the calculated ratios in source_ratios dataset.

--- R/calculate_ratios.R
+++ R/calculate_ratios.R
@@ -7,33 +7,57 @@
 #' @export
 #' @param pah_dat dataframe with PAH concentrations, where there is one column of compounds, and each sample
 #' is contained in a column. If individual samples are to be averaged before computation, the user should supply
-#' the average values.
-#' @return
+#' the average values. The dataframe must contained a column named 'casrn' which contains the casrn numbers
+#' for each compound. Samples below detection limit should be coded as "0" in the concentration column. The function
+#' removes any samples that report a 0 for any of the compounds required to calculate ratios.
+#' @param sample_column name of the column that contains your unique sample identifier
+#' @param conc_column name of the column that contains the results (concentrations)
+#' @return A data frame that contains a column `sample_id` which contains the original sample identifiers,
+#' as well as the unique identifiers of each source contained in `source_profiles`. The source and sample
+#' ratios are defined in the `sample_type` column which is defined as either `source` or `sample`. Additionally,
+#' four columns of ratios are included: Anth_AnthPhen (ratio of anthracene to anthracene plus phenanthrene),
+#' Flua_FluaPyr (ratio of fluoranthene to fluoranthene plus pyrene), Baa_BaaCh (ratio of benzo(a)anthracene
+#' to benzo(a)anthracene plus chrysene), Indpy_IndpyBghip (ratio of indeno(1,2,3-cd)pyrene to
+#' indeno(1,2,3-cd)pyrene plus benzo(g,h,i)perylene).
 #' @import dplyr
-#' @importFrom tidyr gather
+#' @importFrom tidyr spread
 #' @importFrom rlang sym
 #' @examples
 
-pah_profiler <- function(pah_dat, compound_column = 'casrn', sample_column = 'sample_id',
-                         conc_column = 'RESULT', sources = source_profiles) {
-
+calc_ratios <- function(pah_dat, sample_column = 'sample_id', conc_column) {
 
   # make column names dplyr-ready
-  quo_compound_column <- sym(compound_column)
+  quo_sample_column <- sym(sample_column)
+  quo_conc_column <- sym(conc_column)
 
   # remove 0 (BDL) values for compounds that we will use
-  # this includes pcodes 63208, 63227, 63180, 63224, 64111, 63610, 64118, 64114, 64115, 64113
+  # this includes pcodes c(63208, 63180, 64111, 63610, 64118, 64113)
+  # or casrn
+  casrn.keep <- c('206-44-0', '120-12-7', '205-99-2', '56-55-3', '191-24-2')
+  pah_temp <- filter(pah_dat, casrn %in% casrn.keep)
 
-  # create bins based on PAH16 concentration (do this in workflow?)
-  # remove source COA2 - it creates too much dead space -- look at graphs before doing this
-  # compute ratios -- in Austin's version, each compound has its own column
-  ratios$Fl_Py <- ratios$p63208 / ratios$p63227  # flouranthene / pyrene
-  ratios$BbF_BkF <- ratios$p64111 / ratios$p64114  # benzo[b]fluoranthene / benzo[k]fluoranthene
-  ratios$An_178 <- ratios$p63180 / (ratios$p63180 + ratios$p64111) # anthracene / anthracene + phenanthrene (MW178)
-  ratios$Fl_FlPy <- ratios$p63208 / (ratios$p63208 + ratios$p63227) # flouranthene / flouranthene + pyrene (MW202)
-  ratios$BaA_BaACh <- ratios$p63610 / (ratios$p63610 + ratios$p64115) # benz[a]anthracene / benz[a]anthracene + chrysene (MW228)
-  ratios$IP_IPBghiP <- ratios$p64118 / (ratios$p64118 + ratios$p64113)  # indeno[1,2,3-cd]pyrene / indeno[1,2,3-cd]pyrene+ benzo[g,h,i]perylene (MW276)
+  # get a list of sites to remove - that is, those that have conc = 0
+  sites.drop <- unique(pah_temp[, sample_column][pah_temp[, conc_column] == 0])
+  pah_temp <- filter(pah_temp, !((!!quo_sample_column) %in% sites.drop)) %>%
+    select(!!quo_sample_column, casrn, !!quo_conc_column)
 
+  pah_dat_wide <- pah_temp %>%
+    spread(key = casrn, value = !!quo_conc_column)
 
+  pah_dat_wide <- pah_dat_wide %>%
+    mutate(Anth_AnthPhen = `120-12-7`/(`120-12-7` + `85-01-8`),
+           Flua_FluaPyr = `206-44-0`/ (`206-44-0` + `129-00-0`),
+           Indpy_IndpyBghip = `193-39-5`/(`193-39-5` + `191-24-2`),
+           Baa_BaaCh = `56-55-3`/(`56-55-3` + `218-01-9`)) %>%
+    select(!!quo_sample_column, Anth_AnthPhen:Indpy_IndpyBghip) %>%
+    mutate(sample_type = 'sample') %>%
+    rename(sample_id = !!quo_sample_column)
 
+  # Combine sample ratios with source ratios
+  ratios <- rename(source_ratios, sample_id = abbrev) %>%
+    select(sample_id, Anth_AnthPhen:Indpy_IndpyBghip) %>%
+    mutate(sample_type = 'source') %>%
+    bind_rows(pah_dat_wide)
+
+  return(ratios)
 }
