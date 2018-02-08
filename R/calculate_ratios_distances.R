a28cda5c8d862eb53d3d424214a9ea064f8e5528
Oliver <soliver@usgs.gov>
2018-02-08 12:56:38 -0600

    removed parameter summary_type for distinguishing hwo the data are to be returned. Instead, now returns a list of all data summaries, plus the raw data.

--- R/calculate_ratios_distances.R
+++ R/calculate_ratios_distances.R
@@ -6,24 +6,21 @@
 #' @export
 #' @param ratio_dat The output of calc_ratios, which includes the four ratios of interest for both sources
 #' and samples.
-#' @param summary_type Whether the distances should be exported as the raw distances and ranks for each
-#' double ratio plot, source, and sample combination ("raw"),
-#' or summarized by "sample" or by "source". If the data are summarized by source,
-#' the generated table contains a source in each row, and the mean distance and rank between that source
-#' and all samples. This will help discern which sources are closest to the samples overall (or on average). If the distances are summarized
-#' If the data are summarized by sample, the generated table contains a sample in each row, and the top ranked
-#' source for each double ratio comparison. This will help discern the likely source for each sample.
 #' @import dplyr
-#' @return A data frame that contains a column `sample_id` which contains the original sample identifiers,
-#' as well as the unique identifiers of each source contained in `source_profiles`. The source and sample
-#' ratios are defined in the `sample_type` column which is defined as either `source` or `sample`. Additionally,
-#' four columns of ratios are included: Anth_AnthPhen (ratio of anthracene to anthracene plus phenanthrene),
-#' Flua_FluaPyr (ratio of fluoranthene to fluoranthene plus pyrene), Baa_BaaCh (ratio of benzo(a)anthracene
-#' to benzo(a)anthracene plus chrysene), Indpy_IndpyBghip (ratio of indeno(1,2,3-cd)pyrene to
-#' indeno(1,2,3-cd)pyrene plus benzo(g,h,i)perylene).
+#' @return A list of three data frames, which includes 1) distances for each
+#' double ratio plot, source, and sample combination, along with the ranking of each source by sample (1 = closest) for each
+#' double ratio plot ("raw"), 2) distances summarized by source, which contains the mean
+#' distance, rank, and the number of top ranks calculated for each source and double ratio plot combination ("source"),
+#' 3) distances summarized by sample, which reports the top-ranked source for each sample and double
+#' ratio combination ("sample"). Abbreviations for the ratios include: Anth_AnthPhen or AAP (ratio of anthracene
+#' to anthracene plus phenanthrene), Flua_FluaPyr or FFP (ratio of fluoranthene to fluoranthene plus pyrene),
+#' Baa_BaaCh or BBC (ratio of benzo(a)anthracene to benzo(a)anthracene plus chrysene), Indpy_IndpyBghip or IIB (ratio
+#' of indeno(1,2,3-cd)pyrene to indeno(1,2,3-cd)pyrene plus benzo(g,h,i)perylene). Each double ratio comparison
+#' is distinguished by the shorter of the abbreviations separated by an underscore (e.g., FFP_AAP is one double ratio
+#' comparison).
 #' @examples
 
-calc_ratio_dist <- function(ratio_dat, summary_type = 'sample') {
+calc_ratio_dist <- function(ratio_dat) {
 
   structure1 <- select(ratio_dat, sample_id, sample_type) %>%
     filter(sample_type == 'sample') %>%
@@ -61,10 +58,7 @@ calc_ratio_dist <- function(ratio_dat, summary_type = 'sample') {
     group_by(sample) %>%
     mutate(rank1 = row_number(diff1),
            rank2 = row_number(diff2),
-           rank3 = row_number(diff3)) %>%
-    ungroup() %>%
-    rowwise() %>%
-    mutate(sumranks = sum(rank1, rank2, rank3, na.rm = T))
+           rank3 = row_number(diff3))
 
   # find how many times each source is the top source by sample
   # then standardize that by the number of comparisons were made
@@ -81,7 +75,6 @@ calc_ratio_dist <- function(ratio_dat, summary_type = 'sample') {
   by.sources.rank.counts$n_prop <- (rowSums(by.sources.rank.counts[,c('top_diff1', 'top_diff2', 'top_diff3')], na.rm = T)/by.sources.rank.counts$n_poss)*100
   by.sources.rank.counts <- filter(by.sources.rank.counts, !is.na(n_prop))
 
-  if (summary_type == "sources") {
 
   # calculate the mean the distances and ranks for each source
   mean.sources <- ranks %>%
@@ -97,22 +90,18 @@ calc_ratio_dist <- function(ratio_dat, summary_type = 'sample') {
   mean.sources <- left_join(mean.sources, by.sources.rank.counts, by = 'source') %>%
     rename(FFP_AAP_n_toprank = top_diff1, FFP_IIB_n_toprank = top_diff2, BBC_IIB_n_toprank = top_diff3)
 
-  return(mean.sources)
-  } else if (summary_type == "samples") {
+  by.samples <- ranks %>%
+    group_by(sample) %>%
+    summarize(top_source1 = source[which.min(diff1)],
+              top_source2 = source[which.min(diff2)],
+              top_source3 = source[which.min(diff3)])
 
-    by.samples <- ranks %>%
-      group_by(sample) %>%
-      summarize(top_source1 = source[which.min(diff1)],
-                top_source2 = source[which.min(diff2)],
-                top_source3 = source[which.min(diff3)])
+  top.sources <- by.sources$source[by.sources$n_prop > 5]
+  top.sources <- na.omit(top_sources)
 
-    top_sources <- by.sources$source[by.sources$n_prop > 5]
-    top_sources <- na.omit(top_sources)
+  return_dat <- list(ranks, mean.sources, top.sources)
+  names(return_dat) <- c('raw', 'source', 'sample')
 
-    return(top_sources)
-  } else if (summary_type == "raw") {
-    return(ranks)
-  } else {
-    warning("summary_type must be set to 'raw', 'sources', or 'samples'.")
-  }
+  return(return_dat)
+
 }
