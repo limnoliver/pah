7036e3d4281d5dfbd66b27d9f60cc7eb6f7574d7
Oliver <soliver@usgs.gov>
2019-03-25 16:42:37 -0500

    option to override compound metadata table

--- R/get_compound_info.R
+++ R/get_compound_info.R
@@ -15,6 +15,9 @@
 #' (e.g., C2-alkylated naphthalene) do not have
 #' casrn numbers, and metadata on these compounds will not be merged if merge_type = "casrn".
 #' @param merge_col column name that contains the PAH identifier by which to merge
+#' @param compound_info table with compound metadata. A built-in table is the default but may not contain
+#' all compounds in user-supplied dataset. See documentation for the \code{\link{pah_compounds}}
+#' table to see if it fits your needs.
 #' @return A data frame with all of the columns from the user-supplied pah_dat, as well as columns from the
 #' built in data frame pah_compounds. These included...
 #' @export
@@ -23,7 +26,7 @@
 #' @importFrom magrittr %>%
 #' @examples
 
-get_compound_info <- function(pah_dat, merge_type = "name", merge_col = "parameter_nm"){
+get_compound_info <- function(pah_dat, merge_type = "name", merge_col = "parameter_nm", compound_info = pah_compounds){
   temp_dat <- pah_dat
   if (!merge_type %in% c("name", "pcode", "casrn")) {
     stop('merge_type must be set to either "name", "pcode", or "casrn"')
@@ -33,32 +36,32 @@ get_compound_info <- function(pah_dat, merge_type = "name", merge_col = "paramet
   }
 
   if (merge_type == "name") {
-    pah_compounds$Parameter_stripped <- tolower(gsub(pattern = "[[:punct:]]|[[:blank:]]|alkylated|s", replacement = "", pah_compounds$Parameter))
+    compound_info$Parameter_stripped <- tolower(gsub(pattern = "[[:punct:]]|[[:blank:]]|alkylated|s", replacement = "", compound_info$Parameter))
     temp_dat$Parameter_stripped <- tolower(gsub(pattern = "[[:punct:]]|[[:blank:]]|alkylated|s", replacement = "", pah_dat[[merge_col]]))
-    temp_dat <- left_join(temp_dat, pah_compounds, by = "Parameter_stripped") %>%
+    temp_dat <- left_join(temp_dat, compound_info, by = "Parameter_stripped") %>%
       select(-Parameter_stripped)
 
     if (any(is.na(temp_dat$EPApriority16))) {
-      warning(paste0("Not all compound names in column ", merge_col, " matched a compound name in the built-in data frame pah_compounds, so function did not perform a complete merge. Rows with compound names that did not match are included and were given NA values for the new columns."))
+      warning(paste0("Not all compound names in column ", merge_col, " matched a compound name in the built-in data frame compound_info, so function did not perform a complete merge. Rows with compound names that did not match are included and were given NA values for the new columns."))
     }
   }
   if (merge_type == "pcode") {
     if (is.numeric(temp_dat[[merge_col]])) {
-      warning("Pcodes provided are numeric, converted to character string to match data type in pah_compounds")
+      warning("Pcodes provided are numeric, converted to character string to match data type in compound_info")
       temp_dat[merge_col] <- as.character(temp_dat[[merge_col]])
     }
-    temp_dat <- left_join(temp_dat, pah_compounds, by = setNames('pcode', merge_col))
+    temp_dat <- left_join(temp_dat, compound_info, by = setNames('pcode', merge_col))
 
     if (any(is.na(temp_dat$EPApriority16))) {
-      warning(paste0("Not all codes in column ", merge_col, " matched a code in the built-in data frame pah_compounds, so function did not perform a complete merge. Rows with compound names that did not match are included and were given NA values for the new columns."))
+      warning(paste0("Not all codes in column ", merge_col, " matched a code in the built-in data frame compound_info, so function did not perform a complete merge. Rows with compound names that did not match are included and were given NA values for the new columns."))
     }
   }
   if (merge_type == "casrn") {
 
-    temp_dat <- left_join(temp_dat, pah_compounds$pcode, by = setNames("casrn" = merge_col))
+    temp_dat <- left_join(temp_dat, compound_info$pcode, by = setNames("casrn" = merge_col))
 
     if (any(is.na(temp_dat$EPApriority16))) {
-      warning(paste0("Not all codes in column ", merge_col, " matched a code in the built-in data frame pah_compounds, so function did not perform a complete merge. Rows with codes that did not match are included and were given NA values for the new columns."))
+      warning(paste0("Not all codes in column ", merge_col, " matched a code in the built-in data frame compound_info, so function did not perform a complete merge. Rows with codes that did not match are included and were given NA values for the new columns."))
     }
   }
   return(temp_dat)
